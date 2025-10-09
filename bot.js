d94589ff2ee30e00559dcdea4c7653453deb3bd5
Abram-Hortezano <abramjohn13@gmail.com>
2025-10-10 07:04:46 +0800

    new keyword

--- bot.js
+++ bot.js
@@ -40,7 +40,19 @@ client.on(Events.MessageCreate, async (message) => {
 
   if (message.author.bot) return;
 
-  const keywords = ["time in", "time out", "brb", "back"];
+  const keywords = [
+    "time in",
+    "time out",
+    "brb",
+    "back",
+    "out",
+    "in",
+    "bye",
+    "morning",
+    "off",
+    "lunch",
+    "break",
+  ];
   if (keywords.some((k) => message.content.toLowerCase().includes(k))) {
     await axios.post(process.env.MAKE_WEBHOOK_URL, {
       user: message.member?.displayName || message.author.username,
